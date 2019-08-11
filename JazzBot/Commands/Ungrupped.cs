0480ff797ac378532bd6471996c390bd93842af5
N0D4N <disrespectful_mind65@yahoo.com>
2019-08-11 18:19:52 +0300

    Revert "added member's avatar in userinfo command, and small realization fix"
    This reverts commit 46298acf266d55057c0608115962e87d1fc5bf10.

--- JazzBot/Commands/Ungrupped.cs
+++ JazzBot/Commands/Ungrupped.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Globalization;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
@@ -206,15 +205,15 @@ namespace JazzBot.Commands
 
 		private Task<DiscordEmbed> MemberInfo(DiscordMember member, CommandContext context)
 		{
-			var roles = new StringBuilder();
-			if (member.Roles.Any())
+			string roles = "";
+			if (member.Roles.Count() > 0)
 			{
 				var roleslist = member.Roles.OrderByDescending(x => x.Position).Select(x => x.Name).ToList();
 				roleslist.Add("everyone");
-				roles = roles.AppendJoin(',', roleslist.Select(x => $"{Formatter.InlineCode($"@{x}")}"));
+				roles = string.Join(',', roleslist.Select(x => $"{Formatter.InlineCode($"@{x}")}"));
 			}
 			else
-				roles =	roles.AppendLine(Formatter.InlineCode("@everyone"));
+				roles = Formatter.InlineCode("@everyone");
 			return Task.FromResult(EmbedTemplates.ExecutedByEmbed(context.Member, context.Guild.CurrentMember)
 				.WithTitle("Информация об участнике")
 			.AddField("Пользователь", $"{ member.Username}#{member.Discriminator}", true)
@@ -222,8 +221,7 @@ namespace JazzBot.Commands
 			.AddField("Отображаемое имя", member.DisplayName, true)
 			.AddField("Аккаунт создан", $"{(DateTimeOffset.Now - member.CreationTimestamp).Days} дней назад ({member.CreationTimestamp.ToString("dddd, MMM dd yyyy HH:mm:ss zzz", new CultureInfo("ru-Ru"))})", false)
 			.AddField("Участник присоединился", $"{(DateTimeOffset.Now - member.JoinedAt).Days} дней назад ({member.JoinedAt.ToString("dddd, MMM dd yyyy HH:mm:ss zzz", new CultureInfo("ru-Ru"))})", false)
-			.AddField("Роли", roles.ToString(), false)
-			.WithThumbnailUrl(member.AvatarUrl).Build());
+			.AddField("Роли", roles, false).Build());
 		}
 
 		private bool MemberRolePositionAndOwnerChecker(DiscordMember memberInvoked, DiscordMember memberToBan)
